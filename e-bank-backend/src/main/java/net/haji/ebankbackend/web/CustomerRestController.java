e876246d1a336ba1e25a6352ce0a58d23c63adee
mohammedhaji <hmohammediaji@gmail.com>
2025-05-03 19:37:23 +0100

    add rest controller for customer

---
+++ e-bank-backend/src/main/java/net/haji/ebankbackend/web/CustomerRestController.java
@@ -0,0 +1,43 @@
+package net.haji.ebankbackend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.haji.ebankbackend.dtos.CustomerDTO;
+import net.haji.ebankbackend.exceptions.CustomerNotFoundException;
+import net.haji.ebankbackend.services.BankAccountService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/customers")
+    public List<CustomerDTO> customers() {
+        return bankAccountService.listCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public CustomerDTO getCustomer(@PathVariable(name = "customerId") Long customerId) throws CustomerNotFoundException {
+        return bankAccountService.getCustomer(customerId);
+    }
+
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+
+    @PutMapping("/customers/{customerId}")
+    public CustomerDTO updateCustomer(@PathVariable(name = "customerId") Long customerId, @RequestBody CustomerDTO customerDTO) {
+        customerDTO.setId(customerId);
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        bankAccountService.deleteCustomer(id);
+    }
+}
