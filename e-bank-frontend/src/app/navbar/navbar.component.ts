432243c2cca918f9362b9edc394fcf92d7930ca3
mohammedhaji <hmohammediaji@gmail.com>
2025-05-15 08:52:27 +0100

    add navbar style, customer model, and backend logic

--- e-bank-frontend/src/app/navbar/navbar.component.ts
+++ e-bank-frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {Customer} from '../models/customer.model';
+import {CustomerService} from '../services/customer.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  customers! : Array<Customer>;
+
+  constructor(private customerService : CustomerService) {
+  }
+
+  ngOnInit(): void {
+      throw new Error('Method not implemented.');
+  }
 }
